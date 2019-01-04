ddda1124195001725ee346ecda7cf5116585480e
Barry Gitarts <bgitarts@gmail.com>
2019-01-04 15:52:01 -0500

    add snt config to work across networks

--- app/utils/currencies.js
+++ app/utils/currencies.js
@@ -1,5 +1,6 @@
 import StandardToken from 'Embark/contracts/StandardToken'
 import SNT from 'Embark/contracts/SNT'
+import sntIco from 'cryptocurrency-icons/svg/color/snt.svg'
 
 export const TOKEN_ICON_API = 'https://raw.githubusercontent.com/TrustWallet/tokens/master/images'
 export const TOKEN_COIN_API = 'https://raw.githubusercontent.com/TrustWallet/tokens/master/coins'
@@ -12,6 +13,7 @@ export const currencies = [
   {
     value: SNT._address,
     label: 'Status (SNT)',
+    img: sntIco
   },
   {
     value: '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
