4987d8057d4ad9dee7525058d08ff7dde2327746
Barry Gitarts <bgitarts@gmail.com>
2019-01-17 15:38:17 -0500

    add DatabaseProvider

--- app/index.js
+++ app/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import DatabaseProvider from '@nozbe/watermelondb/DatabaseProvider';
+import database from './db';
 import App from './dapp';
 
 render(
-  <App />,
-  document.getElementById('app')
+  <DatabaseProvider database={database}>
+    <App />
+  </DatabaseProvider>, document.getElementById('app')
 );
