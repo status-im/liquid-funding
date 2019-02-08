8b3221b8183be4bd2fb5c27337d9797aa10262f4
Barry Gitarts <bgitarts@gmail.com>
2019-02-08 15:53:37 -0500

    move card styles to own file

---
+++ app/components/table/CardStyles.js
@@ -0,0 +1,24 @@
+import indigo from '@material-ui/core/colors/indigo'
+import blueGrey from '@material-ui/core/colors/blueGrey'
+
+const styles = {
+  card: {
+    borderRadius: '0px',
+    borderTopStyle: 'groove',
+    borderBottom: '1px solid lightgray',
+    backgroundColor: indigo[50]
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  amount: {
+    backgroundColor: blueGrey[50]
+  }
+}
+
+export default styles
