602e32b68ce0b50463a240bfdfac630a15564932
Barry Gitarts <bgitarts@gmail.com>
2019-03-05 16:03:45 -0500

    add BackProject initial grid

--- app/components/projects/BackProject.jsx
+++ app/components/projects/BackProject.jsx
@@ -1,11 +1,32 @@
 import React, { useMemo, useState, useEffect } from 'react'
+import { withStyles } from '@material-ui/core/styles'
 
-function BackProject({match}) {
+const styles = theme => ({
+  root: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(12, [col] 1fr)',
+    gridTemplateRows: 'repeat(5, [row] auto)',
+    gridColumnGap: '1em',
+    gridRowGap: '36px',
+    margin: '1.75rem 4.5rem',
+    fontFamily: theme.typography.fontFamily
+  },
+  title: {
+    fontSize: '2.5rem',
+    gridColumnStart: '3',
+    gridColumnEnd: '10'
+  }
+})
+
+function BackProject({classes, match}) {
   const projectId = match.params.id
   console.log({projectId})
   return (
-    <div>Back Project Page</div>
+    <div className={classes.root}>
+      <div className={classes.title}>Back Project Page</div>
+    </div>
   )
 }
 
-export default BackProject
+const StyledProject = withStyles(styles)(BackProject)
+export default StyledProject
