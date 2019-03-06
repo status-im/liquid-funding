906959959ad16301650fb91532508b6774b610e5
Barry Gitarts <bgitarts@gmail.com>
2019-03-06 14:47:33 -0500

    update loading screen for clarity

--- app/components/base/Loading.jsx
+++ app/components/base/Loading.jsx
@@ -8,6 +8,7 @@ function Loading() {
     <div className="Loading-container">
       <img className="Loading-logo" id="loading" src="https://our.status.im/content/images/2018/07/status_logo_blue_1--2-.png" />
       <Typography style={{ fontSize: '1.5rem' }}>Hold on while we gather the latest information</Typography>
+      <Typography style={{ fontSize: '1.2rem' }}>If this is your first visit it will take about a minute while we sync with the chain</Typography>
     </div>
   )
 }
