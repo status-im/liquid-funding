0b889d1a674d865a0242f711ed2e68b64a0281e1
Barry Gitarts <bgitarts@gmail.com>
2019-03-07 16:05:48 -0500

    add checking if IPFS is ready to be used

--- app/components/projects/hooks.js
+++ app/components/projects/hooks.js
@@ -3,7 +3,8 @@ import web3 from 'Embark/web3'
 import LiquidPledging from 'Embark/contracts/LiquidPledging'
 import { useState, useEffect, useMemo } from 'react'
 import { timeSinceBlock } from '../../utils/dates'
-import { getFiles } from '../../utils/ipfs'
+import { getFiles, ipfs } from '../../utils/ipfs'
+import { databaseExists } from '../../utils/db'
 
 async function getProjectAge(id, events, setState){
   const event = events.find(e => e.returnValues.idProject === id)
@@ -15,13 +16,28 @@ async function getProjectAssets(projectId, setState){
   EmbarkJS.onReady(async (err) => {
     const projectInfo = await LiquidPledging.methods.getPledgeAdmin(projectId).call()
     const CID = projectInfo.url.split('/').slice(-1)[0]
+    console.log({CID, projectInfo, ipfs})
     getFiles(CID)
       .then((files) => {
         setState(files)
         const manifest = files[2]
         console.log({files}, JSON.parse(manifest.content))
       })
-      .catch(console.log)
+      .catch(async (err) => {
+        console.log('IPFS getFiles error: ', err)
+        databaseExists('ipfs')
+          .catch(() => location.reload())
+
+        getFiles(CID)
+          .then((files) => {
+            setState(files)
+            const manifest = files[2]
+            console.log({files}, JSON.parse(manifest.content))
+          })
+          .catch((err) => {
+            console.log('IPFS FAILED ON READY', err)
+          })
+      })
   })
 }
 
@@ -30,6 +46,11 @@ const getProjectManifest = assets => assets ? JSON.parse(assets.find(a => a.name
 export function useProjectData(projectId, profile, projectAddedEvents) {
   const [projectAge, setAge] = useState(null)
   const [projectAssets, setAssets] = useState(null)
+  const [ipfsReady, setIpfsState] = useState(null)
+
+  useEffect(() => {
+    ipfs.on('ready', () => { setIpfsState(true) })
+  }, [projectId])
 
   useEffect(() => {
     getProjectAge(projectId, projectAddedEvents, setAge)
@@ -37,7 +58,7 @@ export function useProjectData(projectId, profile, projectAddedEvents) {
 
   useEffect(() => {
     getProjectAssets(projectId, setAssets)
-  }, [projectId])
+  }, [projectId, ipfsReady])
 
   const manifest = useMemo(() => getProjectManifest(projectAssets), [projectAssets])
 
