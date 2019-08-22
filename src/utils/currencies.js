772e165f7a00811b28f8102db309a56ec8fc8d42
Barry Gitarts <bgitarts@gmail.com>
2019-08-22 11:52:50 -0400

    reorder currencies

--- src/utils/currencies.js
+++ src/utils/currencies.js
@@ -11,6 +11,23 @@ export const TOKEN_COIN_API = 'https://raw.githubusercontent.com/TrustWallet/tok
 export const TOKEN_API = 'https://raw.githubusercontent.com/TrustWallet/tokens/master/tokens'
 export const currencies = [
   {
+    value: SNT._address,
+    label: 'SNT',
+    img: sntIco,
+    contract: SNT,
+    humanReadibleFn: toEther,
+    chainReadibleFn: toWei
+  },
+  {
+    value: '0xf5dce57282a584d2746faf1593d3121fcac444dc',
+    label: 'cDAI',
+    img: `${TOKEN_API}/0xf5dce57282a584d2746faf1593d3121fcac444dc.png`,
+    width: '5%',
+    contract: cDAI,
+    humanReadibleFn: compoundWhole,
+    chainReadibleFn: compoundToChain
+  },
+  {
     value: '0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5',
     label: 'cETH',
     img: `${TOKEN_API}/0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5.png`,
@@ -28,14 +45,6 @@ export const currencies = [
     chainReadibleFn: toWei
   },
   {
-    value: SNT._address,
-    label: 'SNT',
-    img: sntIco,
-    contract: SNT,
-    humanReadibleFn: toEther,
-    chainReadibleFn: toWei
-  },
-  {
     value: '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
     label: 'DAI',
     img: `${TOKEN_API}/0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359.png`,
@@ -43,15 +52,6 @@ export const currencies = [
     contract: DAI,
     humanReadibleFn: toEther,
     chainReadibleFn: toWei
-  },
-  {
-    value: '0xf5dce57282a584d2746faf1593d3121fcac444dc',
-    label: 'cDAI',
-    img: `${TOKEN_API}/0xf5dce57282a584d2746faf1593d3121fcac444dc.png`,
-    width: '5%',
-    contract: cDAI,
-    humanReadibleFn: compoundWhole,
-    chainReadibleFn: compoundToChain
   }
 ]
 
