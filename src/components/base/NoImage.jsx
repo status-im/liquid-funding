b68da353789741348fad9345f34aa479ea39f0ce
Barry Gitarts <bgitarts@gmail.com>
2019-08-23 08:38:28 -0400

    add no image placeholder

---
+++ src/components/base/NoImage.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import PhotoIcon from './icons/Photo.jsx'
+
+const styles = theme => ({
+  theme,
+  main: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    background: 'rgba(147, 155, 161, 0.1)',
+    minHeight: '15em',
+    gridColumn: '1 / 13'
+  },
+  icon: {},
+  subText: {
+    color: '#939BA1',
+    fontSize: '0.9375rem'
+  }
+})
+
+function NoImage({ classes }){
+  return (
+    <div className={classes.main}>
+      <div className={classes.icon}>
+        <PhotoIcon />
+      </div>
+      <div className={classes.subText}>No image available</div>
+    </div>
+  )
+}
+
+export default withStyles(styles)(NoImage)
