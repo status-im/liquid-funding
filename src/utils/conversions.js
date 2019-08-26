d07507773dd7e842472ef55615ecc70082ec13e1
Barry Gitarts <bgitarts@gmail.com>
2019-08-26 14:15:29 -0400

    add optimistic update

--- src/utils/conversions.js
+++ src/utils/conversions.js
@@ -2,5 +2,6 @@
 
 export const toEther = (amount, scale = 'ether') => web3.utils.fromWei(amount, scale)
 export const toWei = (amount, scale = 'ether') => web3.utils.toWei(amount, scale)
+export const toBN = amount => web3.utils.toBN(amount)
 export const compoundWhole = amount => (Number(amount) / (10**8)).toString()
 export const compoundToChain = amount => (Number(amount) * (10**8)).toString()
