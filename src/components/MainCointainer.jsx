e6b6737242923f1d6a34cef228db1c85b2c9dcce
Barry Gitarts <bgitarts@gmail.com>
2019-08-30 08:25:02 -0400

    add scroll to top on route change

--- src/components/MainCointainer.jsx
+++ src/components/MainCointainer.jsx
@@ -157,6 +157,20 @@ function PersistentDrawerLeft({ loading, account, children, enableEthereum, loca
   const position = useWindowScrollPosition()
 
   useEffect(() => {
+    try {
+      // trying to use new API - https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollTo
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'smooth',
+      });
+    } catch (error) {
+      // just a fallback for older browsers
+      window.scrollTo(0, 0);
+    }
+  }, [pathname, search])
+
+  useEffect(() => {
     const queryParams = queryString.parse(search)
     if (queryParams.logs) enableLogs()
     setQueryParams({ queryParams })
