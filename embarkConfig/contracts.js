d9856c3323daa4413c467af0ed3de6dab25af046
Barry Gitarts <bgitarts@gmail.com>
2019-10-22 14:50:24 -0400

    update SwapProxy to update key variables
    proper slippage calc on token transfers

--- embarkConfig/contracts.js
+++ embarkConfig/contracts.js
@@ -184,7 +184,7 @@ module.exports = {
         address: "0x8aA3672a99C489E5Dc5dfDb40e607bE49970cbF7"
       },
       SwapProxy: {
-        address: "0x46A30Bf6D0E438E7EeaabD6c4B5fEBf481267722",
+        address: "0x1B468F2a4CDaC05E5B2f432DA24F049201B74Aa2",
         args: [
           '$LiquidPledging',
           "0x818E6FECD516Ecc3849DAf6845e3EC868087B755",
