0cbf15859baaaee0982f4eecb6d9c65cc7053176
Barry Gitarts <bgitarts@gmail.com>
2019-11-18 14:29:49 -0500

    replace status logo with assemble

--- src/components/base/Loading.jsx
+++ src/components/base/Loading.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
+import assemble from '../../images/assemble-high-res.png'
 
 import './Loading.css'
 
 function Loading() {
   return (
     <div className="Loading-container">
-      <img className="Loading-logo" id="loading" src="https://our.status.im/content/images/2018/07/status_logo_blue_1--2-.png" />
+      <img className="Loading-logo" id="loading" src={assemble} />
       <Typography style={{ fontSize: '1.5rem' }}>Hold on while we gather the latest information</Typography>
       <Typography style={{ fontSize: '1.2rem' }}>If this is your first visit it will take about a minute while we sync with the chain</Typography>
     </div>
