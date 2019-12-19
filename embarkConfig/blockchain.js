a799d8dcacfc4e83f429e318a02eb69d190ee5b2
Pascal Precht <pascal.precht@gmail.com>
2019-12-19 11:24:37 +0100

    chore: upgrade to Embark v5

--- embarkConfig/blockchain.js
+++ embarkConfig/blockchain.js
@@ -1,37 +1,34 @@
+let secret = {};
+try {
+  secret = require('../.secret.json');
+} catch(err) {
+  console.dir("warning: .secret.json file not found; this is only needed to deploy to testnet or livenet etc..");
+}
+
 module.exports = {
   // applies to all environments
   default: {
     enabled: true,
-    rpcHost: "localhost", // HTTP-RPC server listening interface (default: "localhost")
-    rpcPort: 8545, // HTTP-RPC server listening port (default: 8545)
-    rpcCorsDomain: "auto",  // Comma separated list of domains from which to accept cross origin requests (browser enforced)
-                            // When set to "auto", Embark will automatically set the cors to the address of the webserver
-    wsRPC: true, // Enable the WS-RPC server
-    wsOrigins: "auto",  // Origins from which to accept websockets requests
-                        // When set to "auto", Embark will automatically set the cors to the address of the webserver
-    wsHost: "localhost", // WS-RPC server listening interface (default: "localhost")
-    wsPort: 8546 // WS-RPC server listening port (default: 8546)
+    client: "geth",
+    accounts: [
+      {
+        nodeAccounts: true,
+        numAddresses: 1,
+        password: "embarkConfig/development/devpassword"
+      }
+    ]
   },
 
   // default environment, merges with the settings in default
   // assumed to be the intended environment by `embark run` and `embark blockchain`
   development: {
-    ethereumClientName: "geth", // Can be geth or parity (default:geth)
-    //ethereumClientBin: "geth",  // path to the client binary. Useful if it is not in the global PATH
-    networkType: "custom", // Can be: testnet, rinkeby, livenet or custom, in which case, it will use the specified networkId
-    networkId: 1337, // Network id used when networkType is custom
-    isDev: true, // Uses and ephemeral proof-of-authority network with a pre-funded developer account, mining enabled
-    datadir: ".embark/development/datadir", // Data directory for the databases and keystore (Geth 1.8.15 and Parity 2.0.4 can use the same base folder, till now they does not conflict with each other)
-    mineWhenNeeded: true, // Uses our custom script (if isDev is false) to mine only when needed
-    nodiscover: true, // Disables the peer discovery mechanism (manual peer addition)
-    maxpeers: 0, // Maximum number of network peers (network disabled if set to 0) (default: 25)
-    proxy: true, // Proxy is used to present meaningful information about transactions
-    targetGasLimit: 8000000, // Target gas limit sets the artificial target gas floor for the blocks to mine
-    simulatorBlocktime: 0, // Specify blockTime in seconds for automatic mining. Default is 0 and no auto-mining.
+    clientConfig: {
+      miningMode: 'dev'
+    },
     accounts: [
       {
         nodeAccounts: true,
-        numAddresses: "1",
+        numAddresses: 1,
         password: "embarkConfig/development/devpassword"
       }
     ]
@@ -40,63 +37,26 @@ module.exports = {
   // merges with the settings in default
   // used with "embark run privatenet" and/or "embark blockchain privatenet"
   privatenet: {
-    networkType: "custom",
-    networkId: 1337,
-    isDev: false,
-    datadir: ".embark/privatenet/datadir",
-    // -- mineWhenNeeded --
-    // This options is only valid when isDev is false.
-    // Enabling this option uses our custom script to mine only when needed.
-    // Embark creates a development account for you (using `geth account new`) and funds the account. This account can be used for
-    // development (and even imported in to MetaMask). To enable correct usage, a password for this account must be specified
-    // in the `account > password` setting below.
-    // NOTE: once `mineWhenNeeded` is enabled, you must run an `embark reset` on your dApp before running
-    // `embark blockchain` or `embark run` for the first time.
-    mineWhenNeeded: true,
-    // -- genesisBlock --
-    // This option is only valid when mineWhenNeeded is true (which is only valid if isDev is false).
-    // When enabled, geth uses POW to mine transactions as it would normally, instead of using POA as it does in --dev mode.
-    // On the first `embark blockchain or embark run` after this option is enabled, geth will create a new chain with a
-    // genesis block, which can be configured using the `genesisBlock` configuration option below.
-    genesisBlock: "embarkConfig/privatenet/genesis.json", // Genesis block to initiate on first creation of a development node
-    nodiscover: true,
-    maxpeers: 0,
-    proxy: true,
     accounts: [
       {
         nodeAccounts: true,
-        numAddresses: "1",
         password: "embarkConfig/privatenet/password"
       }
     ],
-    targetGasLimit: 8000000,
-    wsHost: "localhost",
-    wsPort: 8546,
-    simulatorBlocktime: 0
+    clientConfig: {
+      datadir: ".embark/privatenet/datadir",
+      miningMode: 'auto',
+      genesisBlock: "embarkConfig/privatenet/genesis.json", // Genesis block to initiate on first creation of a development node
+    }
   },
 
     // merges with the settings in default
   // used with "embark run privatparityenet" and/or "embark blockchain privateparitynet"
   privateparitynet: {
-    ethereumClientName: "parity",
-    networkType: "custom",
-    networkId: 1337,
-    isDev: false,
-    genesisBlock: "embarkConfig/privatenet/genesis-parity.json", // Genesis block to initiate on first creation of a development node
-    datadir: ".embark/privatenet/datadir", // (Geth 1.8.15 and Parity 2.0.4 can use the same base folder, till now they does not conflict with each other)
-    mineWhenNeeded: false,
-    nodiscover: true,
-    maxpeers: 0,
-    proxy: true,
-    accounts: [
-      {
-        nodeAccounts: true,
-        numAddresses: "1",
-        password: "embarkConfig/privatenet/password"
-      }
-    ],
-    targetGasLimit: 8000000,
-    simulatorBlocktime: 0
+    client: "parity",
+    genesisBlock: "embarkConfig/privatenet/genesis-parity.json",
+    datadir: ".embark/privatenet/datadir",
+    miningMode: 'off'
   },
 
   // merges with the settings in default
@@ -112,6 +72,7 @@ module.exports = {
       }
     ]
   },
+
   rinkeby: {
     networkType: "rinkeby",
     syncMode: "light",
@@ -124,20 +85,28 @@ module.exports = {
     ]
   },
 
+  rinkebyInfura: {
+    endpoint: `https://rinkeby.infura.io/${secret.infuraKey}`,
+    accounts: [
+      {
+        mnemonic: secret.mnemonic,
+        numAddresses: 10
+      }
+    ]
+  },
+
   // merges with the settings in default
   // used with "embark run livenet" and/or "embark blockchain livenet"
   livenet: {
     networkType: "livenet",
     syncMode: "light",
-    rpcCorsDomain: "http://localhost:8000",
-    wsOrigins: "http://localhost:8000",
+    endpoint: "https://mainnet.infura.io/v3/a2687d7078ff46d3b5f3f58cb97d3e44",
     accounts: [
       {
-        nodeAccounts: true,
-        numAddresses: "1",
-        password: "embarkConfig/livenet/password"
+        privateKeyFile: secret.privateKeyFile,
+        password: secret.password
       }
-    ]
+    ],
   }
 
   // you can name an environment with specific settings and then specify with
