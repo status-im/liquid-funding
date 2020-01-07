312396f8e3df091fc8b9b76b2577b611b92639bd
Barry Gitarts <bgitarts@gmail.com>
2020-01-07 12:28:38 -0500

    display all pledges in pledges view

--- src/components/projects/queries.js
+++ src/components/projects/queries.js
@@ -79,7 +79,7 @@ query Profile($id: ID!) {
     pledgesInfos {
       ...PledgesInfoFields
     }
-    pledges(where: {amount_gt: 0, pledgeState_lt: 1}) {
+    pledges(where: {amount_gt: 0}) {
       ...PledgeFields
     }
     projectInfo {
